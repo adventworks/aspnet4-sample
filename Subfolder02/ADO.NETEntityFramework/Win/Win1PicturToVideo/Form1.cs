2a4eaedb49e2cbbc3ad516f11e39d7fe1cbac09a
Test01 <2969459391@qq.com>
2019-02-22 15:37:18 +0800

    Test upload repo

---
+++ Subfolder02/ADO.NETEntityFramework/Win/Win1PicturToVideo/Form1.cs
@@ -0,0 +1,162 @@
+﻿using SharpAvi;
+using SharpAvi.Output;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win1PicturToVideo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        public int PictureNumber = 0;
+        public int CaptureInterval = 100;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            button2.Enabled = true;
+            button1.Enabled = false;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PictureNumber = 0;
+            timer1.Stop();
+            timer2.Stop();
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CaptureInterval += 100;
+            label2.Text = CaptureInterval.ToString();
+            timer1.Interval = CaptureInterval;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (CaptureInterval > 100)
+            {
+                CaptureInterval -= 100;
+                label2.Text = CaptureInterval.ToString();
+                timer1.Interval = CaptureInterval;
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            PictureNumber += 1;
+            SendKeys.Send("{PRTSC}");
+            //pictureBox1.Image = null;
+            Image img = Clipboard.GetImage();
+            img.Save("D:\\Pictures\\image" + PictureNumber + ".jpg");
+            pictureBox1.Image = img;
+        }
+        private void Play()
+        {
+            string[] allFile = Directory.GetFiles("D:\\Pictures");
+            foreach (var item in allFile)
+            {
+                //File file = new FileInfo();
+                Image image = Image.FromFile(item);
+                pictureBox1.Image = image;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetFiles(@"C:\Users\v-jianpw\Desktop\New folder");
+            //AviWriter aviWrite = new AviWriter(files[0]);
+            GenerateSingleImageVideo(files[4]);
+            
+        }
+
+        private void GenerateSingleImageVideo(string filePath)
+        {
+            string imagePath = filePath;
+            Bitmap thisBitmap;
+
+            //generate bitmap from image file
+            using (Stream BitmapStream = System.IO.File.Open(imagePath, FileMode.Open))
+            {
+                Image img = Image.FromStream(BitmapStream);
+                thisBitmap = new Bitmap(img);
+            }
+
+            //convert the bitmap to a byte array
+            byte[] byteArray = BitmapToByteArray(thisBitmap);
+
+            //creates the writer of the file (to save the video)
+            var writer = new AviWriter(@"C:\Users\v-jianpw\Desktop\New folder\11.avi");
+            //writer.FramesPerSecond = byteArray.Length;
+            var stream = writer.AddVideoStream();
+            stream.Width = thisBitmap.Width;
+            stream.Height = thisBitmap.Height;
+            stream.Codec = KnownFourCCs.Codecs.Uncompressed;
+            stream.BitsPerPixel = BitsPerPixel.Bpp32;
+
+            int numberOfFrames = 1 * 100;// ((int.Parse("1.0")) * (int.Parse("100.0")));
+            int count = 0;
+
+            while (count <= 10)
+            {
+                stream.WriteFrame(true, byteArray, 0, byteArray.Length);
+                count++;
+            }
+           
+            writer.Close();
+            MessageBox.Show("Done");
+
+
+        }
+
+        private byte[] BitmapToByteArray(Bitmap img)
+        {
+            ImageConverter converter = new ImageConverter();
+            return (byte[])converter.ConvertTo(img, typeof(byte[]));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string str = "St1TUzIxAAADnpwECAUHCc7QAAAbn2kBAAAAg0MdYp45APgPmgCHAAmR";
+            byte[] bytes = Encoding.Unicode.GetBytes(str);
+            
+            int columns =20;
+            int rows = 5;
+            int stride = columns;
+            byte[] newbytes = PadLines(bytes, rows, columns);
+
+            Bitmap im = new Bitmap(columns, rows, stride,
+                     PixelFormat.Format8bppIndexed,
+                     Marshal.UnsafeAddrOfPinnedArrayElement(newbytes, 0));
+
+            im.Save(@"C:\Users\v-jianpw\Desktop\New folder\image21.jpg");
+        }
+        static byte[] PadLines(byte[] bytes, int rows, int columns)
+        {
+            int currentStride = columns; // 3
+            int newStride = columns;  // 4
+            byte[] newBytes = new byte[newStride * rows];
+            for (int i = 0; i < rows; i++)
+                Buffer.BlockCopy(bytes, currentStride * i, newBytes, newStride * i, currentStride);
+            return newBytes;
+        }
+    }
+}
---
+++ Subfolder02/ADO.NETEntityFramework02/ConsoleApplication1/LINQRecursive/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQRecursive
+{
+    class Program
+    {
+        public static IQueryable<Provice> Provices;
+        static void Main(string[] args)
+        {
+            IEnumerable<Provice> provices = new Provice[] {
+                new Provice { ParentItemKey="China",ItemKey="1",ItemValue="Xi'an"},
+                new Provice { ParentItemKey="China",ItemKey="2",ItemValue="Bei'jing"},
+                new Provice { ParentItemKey="China",ItemKey="3",ItemValue="Shang'hai"},
+                new Provice { ParentItemKey="1",ItemKey="1",ItemValue="a"},
+                new Provice { ParentItemKey="1",ItemKey="2",ItemValue="b"},
+                new Provice { ParentItemKey="2",ItemKey="3",ItemValue="c"},
+                new Provice { ParentItemKey="2",ItemKey="4",ItemValue="d"},
+                new Provice { ParentItemKey="3",ItemKey="5",ItemValue="e"},
+                new Provice { ParentItemKey="3",ItemKey="6",ItemValue="f"}
+                //new Provice { ParentItemKey="China",ItemKey="1",ItemValue="Xi'an"},
+                //new Provice { ParentItemKey="China",ItemKey="2",ItemValue="Bei'jing"},
+                //new Provice { ParentItemKey="China",ItemKey="3",ItemValue="Shang'hai"},
+                //new Provice { ParentItemKey="1",ItemKey="a",ItemValue="aa"},
+                //new Provice { ParentItemKey="1",ItemKey="b",ItemValue="bb"},
+                //new Provice { ParentItemKey="2",ItemKey="c",ItemValue="cc"},
+                //new Provice { ParentItemKey="2",ItemKey="d",ItemValue="dd"},
+                //new Provice { ParentItemKey="3",ItemKey="e",ItemValue="ee"},
+                //new Provice { ParentItemKey="3",ItemKey="f",ItemValue="ff"},
+                //new Provice { ParentItemKey="a",ItemKey="aa",ItemValue="abc"},
+                //new Provice { ParentItemKey="b",ItemKey="bb",ItemValue="def"},
+                //new Provice { ParentItemKey="c",ItemKey="cc",ItemValue="ghk"},
+            };
+            Provices = provices.AsQueryable();
+
+            var item = Provices
+                .Where(p => p.ParentItemKey == "1");
+            //.Select(c=>new {
+            //    ParentItemKey=c.ParentItemKey,
+            //    ItemKey=c.ItemKey,
+            //    ItemValue=c.ItemValue
+            //}).AsQueryable();
+
+
+            HieararchyWalk(item);
+
+            //List<Provice> list = GetItemTree(item).ToList();
+            Console.ReadKey();
+        }
+       
+        
+
+        public static void HieararchyWalk(IQueryable<Provice> parentList)
+        {
+            if (parentList != null)
+            {
+                foreach (var item in parentList)
+                {
+                    Console.WriteLine(string.Format("{0}       {1}", item.ItemKey, item.ItemValue));
+                    var children = Provices.Where(s => parentList.Any(p => p.ItemKey == item.ParentItemKey));
+
+                    ///var children=
+                    HieararchyWalk(children);
+                }
+            }
+        }
+    }
+    public class Provice
+    {
+        public string ParentItemKey { set; get; }
+        public string ItemKey { set; get; }
+        public string ItemValue { set; get; }
+
+        public IQueryable<Provice> Children { set; get; }
+    }
+
+    #region
+    //public class Comment
+    //{
+    //    public int Id { get; set; }
+    //    public int ParentId { get; set; }
+    //    public string Text { get; set; }
+    //    public List<Comment> Children { get; set; }
+    //}
+
+    //class Program
+    //{
+    //    static void Main()
+    //    {
+    //        List<Comment> categories = new List<Comment>()
+    //        {
+    //            new Comment () { Id = 1, Text = "Item 1", ParentId = 0},
+    //            new Comment() { Id = 2, Text = "Item 2", ParentId = 0 },
+    //            new Comment() { Id = 3, Text = "Item 3", ParentId = 0 },
+    //            new Comment() { Id = 4, Text = "Item 1.1", ParentId = 1 },
+    //            new Comment() { Id = 5, Text = "Item 3.1", ParentId = 3 },
+    //            new Comment() { Id = 6, Text = "Item 1.1.1", ParentId = 4 },
+    //            new Comment() { Id = 7, Text = "Item 2.1", ParentId = 2 }
+    //        };
+
+    //        List<Comment> hierarchy = new List<Comment>();
+    //        hierarchy = categories
+    //                        .Where(c => c.ParentId == 0)
+    //                        .Select(c => new Comment()
+    //                        {
+    //                            Id = c.Id,
+    //                            Text = c.Text,
+    //                            ParentId = c.ParentId,
+    //                            Children = GetChildren(categories, c.Id)
+    //                        })
+    //                        .ToList();
+
+    //        HieararchyWalk(hierarchy);
+
+    //        Console.ReadLine();
+    //    }
+
+    //    public static List<Comment> GetChildren(List<Comment> comments, int parentId)
+    //    {
+    //        return comments
+    //                .Where(c => c.ParentId == parentId)
+    //                .Select(c => new Comment
+    //                {
+    //                    Id = c.Id,
+    //                    Text = c.Text,
+    //                    ParentId = c.ParentId,
+    //                    Children = GetChildren(comments, c.Id)
+    //                })
+    //                .ToList();
+    //    }
+
+    //    public static void HieararchyWalk(List<Comment> hierarchy)
+    //    {
+    //        if (hierarchy != null)
+    //        {
+    //            foreach (var item in hierarchy)
+    //            {
+    //                Console.WriteLine(string.Format("{0} {1}", item.Id, item.Text));
+    //                HieararchyWalk(item.Children);
+    //            }
+    //        }
+    //    }
+    //}
+    #endregion
+}
